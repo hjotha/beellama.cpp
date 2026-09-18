4f0f84575c9edafa2ba95bd2e3dc09c8df360be4
hjotha <hjotha@gmail.com>
2026-09-18 12:59:47 +0200

    fix(build): drop duplicate type_traits closer in ggml.c merge

--- ggml/src/ggml.c
+++ ggml/src/ggml.c
@@ -754,7 +754,6 @@ static const struct ggml_type_traits type_traits[GGML_TYPE_COUNT] = {
         .to_float                 = (ggml_to_float_t) dequantize_row_ptq1_0,
         .from_float_ref           = (ggml_from_float_t) quantize_row_ptq1_0_ref,
     },
-    },
     [GGML_TYPE_Q4_0] = {
         .type_name                = "q4_0",
         .blck_size                = QK4_0,
